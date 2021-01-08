e875ce9bb9afae3fad48db21c0d221f2c2f8d276
tianqizhemere <823507949@qq.com>
2021-01-08 11:59:20 +0800

    封装统一响应结果

--- plankton-web-service/src/main/java/top/tianqi/plankton/system/controller/UserController.java
+++ plankton-web-service/src/main/java/top/tianqi/plankton/system/controller/UserController.java
@@ -3,6 +3,7 @@ package top.tianqi.plankton.system.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.tianqi.plankton.common.Result;
 import top.tianqi.plankton.common.annotation.aop.OperLog;
 import top.tianqi.plankton.common.base.BaseController;
 import top.tianqi.plankton.common.constant.OperationConst;
@@ -21,7 +22,7 @@ public class UserController extends BaseController {
      */
     @OperLog(operationModel = "用户管理", operationDesc = OperationConst.SELECT, operationType = "查询用户列表")
     @GetMapping(value = "/list")
-    public Message list(){
-        return SUCCESS_MESSAGE;
+    public Result list(){
+        return SUCCESS_MESSAGE();
     }
 }
