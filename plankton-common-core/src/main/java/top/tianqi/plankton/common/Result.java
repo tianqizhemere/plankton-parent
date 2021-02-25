2bc1433b54cc8d9fbc15d3275e2af5c36a5fdd33
tianqizhemere <823507949@qq.com>
2021-02-25 14:38:40 +0800

    修改Result统一响应体类

--- plankton-common-core/src/main/java/top/tianqi/plankton/common/Result.java
+++ plankton-common-core/src/main/java/top/tianqi/plankton/common/Result.java
@@ -13,24 +13,17 @@ import java.util.HashMap;
 public class Result extends HashMap<String, Object> {
 
     private static final long serialVersionUID = -3990768377677695712L;
-    /**
-     * 状态码
-     */
+
+    /** 状态码 */
     private static final String CODE_TAG = "code";
 
-    /**
-     * 返回内容
-     */
+    /** 返回内容 */
     private static final String MSG_TAG = "massage";
 
-    /**
-     * 数据对象
-     */
+    /** 数据对象 */
     private static final String DATA_TAG = "data";
 
-    /**
-     * 是否成功
-     */
+    /** 是否成功 */
     private static final String IS_SUCCESS = "success";
 
     /**
@@ -63,6 +56,21 @@ public class Result extends HashMap<String, Object> {
         super.put(MSG_TAG, msg);
         super.put(DATA_TAG, data);
         super.put(IS_SUCCESS, Boolean.TRUE);
+    }
+
+    /**
+     * 初始化一个新创建的 Result 对象
+     *
+     * @param code 状态码
+     * @param msg  返回内容
+     * @param data 数据对象
+     * @param isSuccess 是否操作成功
+     */
+    public Result(int code, String msg, Object data, boolean isSuccess) {
+        super.put(CODE_TAG, code);
+        super.put(MSG_TAG, msg);
+        super.put(DATA_TAG, data);
+        super.put(IS_SUCCESS, isSuccess);
     }
 
     /**
@@ -101,7 +109,7 @@ public class Result extends HashMap<String, Object> {
      * @return 成功消息
      */
     public static Result success(String msg, Object data) {
-        return new Result(HttpStatus.SC_OK, msg, data);
+        return new Result(HttpStatus.SC_OK, msg, data, Boolean.TRUE);
     }
 
     /**
@@ -118,9 +126,7 @@ public class Result extends HashMap<String, Object> {
      * @return 警告消息
      */
     public static Result error(String msg) {
-        Result error = Result.error(msg, null);
-        error.put(IS_SUCCESS,  Boolean.FALSE);
-        return error;
+        return Result.error(msg, null);
     }
 
     /**
@@ -131,9 +137,7 @@ public class Result extends HashMap<String, Object> {
      * @return 警告消息
      */
     public static Result error(String msg, Object data) {
-        Result result = new Result(HttpStatus.SC_INTERNAL_SERVER_ERROR, msg, data);
-        result.put(IS_SUCCESS, Boolean.FALSE);
-        return result;
+        return new Result(HttpStatus.SC_INTERNAL_SERVER_ERROR, msg, data, Boolean.FALSE);
     }
 
     /**
@@ -144,9 +148,7 @@ public class Result extends HashMap<String, Object> {
      * @return 警告消息
      */
     public static Result error(int code, String msg) {
-        Result result = new Result(code, msg, null);
-        result.put(IS_SUCCESS, Boolean.FALSE);
-        return result;
+        return new Result(code, msg, null, Boolean.FALSE);
     }
 
     /**
@@ -156,9 +158,7 @@ public class Result extends HashMap<String, Object> {
      * @return 警告消息
      */
     public static Result error(ErrorStateEnum errorState) {
-        Result error = Result.error(errorState.getCode(), errorState.getMsg());
-        error.put(IS_SUCCESS, Boolean.FALSE);
-        return error;
+        return Result.error(errorState.getCode(), errorState.getMsg());
     }
 
     /**
@@ -169,8 +169,6 @@ public class Result extends HashMap<String, Object> {
      * @return 警告消息
      */
     public static Result error(ErrorStateEnum errorState, Object data) {
-        Result result = new Result(errorState.getCode(), errorState.getMsg(), data);
-        result.put(IS_SUCCESS, Boolean.FALSE);
-        return result;
+        return new Result(errorState.getCode(), errorState.getMsg(), data, Boolean.FALSE);
     }
 }
