40fbc820777dd4f0ccaf0755e36a55b49a7e92e3
tianqizhemere <823507949@qq.com>
2021-03-13 13:05:27 +0800

    回滚updateById方法

--- plankton-web-service/src/main/java/top/tianqi/plankton/system/service/impl/VersionServiceImpl.java
+++ plankton-web-service/src/main/java/top/tianqi/plankton/system/service/impl/VersionServiceImpl.java
@@ -26,6 +26,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * 版本检测服务层实现
@@ -182,6 +183,28 @@ public class VersionServiceImpl extends BaseServiceImpl<VersionMapper, VersionIn
             }
         }
         return true;
+    }
+
+    @Override
+    public boolean updateById(VersionInfo versionInfo) {
+        if (!StringUtils.isEmpty(versionInfo.getAttachIds())) {
+            // 先清除之前关联的附件
+            List<Attach> fileList = attachMapper.findList(versionInfo.getId(), String.valueOf(AttachDataTypeEnum.MODEL_APPLICATION.getCode()));
+            if (!CollectionUtils.isEmpty(fileList)) {
+                List<Long> attachIds = fileList.stream().map(Attach::getId).collect(Collectors.toList());
+                attachMapper.deleteBatchIds(attachIds);
+            }
+            for (String attachId : versionInfo.getAttachIds().split(",")) {
+                Attach attach = attachMapper.selectById(new Long(attachId));
+                if (attach != null) {
+                    attach.setRecordId(versionInfo.getId());
+                    attach.setDataType(AttachDataTypeEnum.MODEL_APPLICATION.getCode());
+                    attachMapper.insert(attach);
+                    versionInfo.setDownloadUrl(attach.getPath());
+                }
+            }
+        }
+        return super.updateById(versionInfo);
     }
 
     /**
